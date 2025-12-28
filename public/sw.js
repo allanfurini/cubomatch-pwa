6add83719278796c9e5910ea4313e8e16ef2643c
ALLAN ALVES FURINI XAVIER <32076896+allanfurini@users.noreply.github.com>
2025-12-28 16:48:47 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,20 @@
+const CACHE = "cubomatch-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/competitor.html",
+  "/app.js",
+  "/pwa.js",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
 
